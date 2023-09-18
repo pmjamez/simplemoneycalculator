212b34d26f768e4b326b716de032fa3e37d7c9c0
James <101947821+pmjamez@users.noreply.github.com>
2023-09-17 20:13:04 -0700

    major changes to layout, added title, and filled in input boxes

--- frontend/static/js/views/salary.js
+++ frontend/static/js/views/salary.js
@@ -10,35 +10,7 @@ export default class extends AbstractView{
     async getHtml(){
         return `
 
-        <body>
-
-          <div class="rounded-rectangle">
-            <h2 class = "title">Calculate Salary</h2>
-            <p class="description">Calculate your estimated tax payments based on your salary</p>
-
-            <div class="input-container">
-                <label for="tax-input" class="input-label">Tax Amount:</label>
-                <input type="text" id="tax-input" class="input-box" placeholder="Enter text...">
-
-                <label for="tax-input" class="input-label">Something:</label>
-                <input type="text" id="tax-input" class="input-box" placeholder="Enter text...">
-
-                <button class="blue-button">Submit</button>
-                
-            </div>
-
-        </div>
-
-        <div class = "rounded-rectangle2">
-
-          </div>
-            
-          
-  
-    
-       
-
-         </body>
+        
       
     `
         
--- frontend/static/js/views/tax.js
+++ frontend/static/js/views/tax.js
@@ -9,31 +9,54 @@ export default class extends AbstractView{
 
     async getHtml(){
         return `
-
         <body>
 
-          <div class="rounded-rectangle">
-            <h2 class = "title">Calculate Tax</h2>
-            <p class="description">Calculate your estimated tax payments based on your salary</p>
+        <h1 class="page-title">QUICK CASH</h1>
 
-            <div class="input-container">
-                <label for="tax-input" class="input-label">Tax Amount:</label>
-                <input type="text" id="tax-input" class="input-box" placeholder="Enter text...">
+    
+          <nav class = "nav">
+                <a href = "/tax" class = "nav__link" data-link > Tax Calculator </a>
+                <a href = "/salary" class = "nav__link" data-link> Salary Calculator</a>
+           </nav>
+         </div>
 
-                <label for="tax-input" class="input-label">Something:</label>
-                <input type="text" id="tax-input" class="input-box" placeholder="Enter text...">
+         <div class = "pagebox">
+             <div class="rounded-rectangle">
+             <h2 class = "title">Calculate Tax</h2>
+             <p class="description">Calculate your estimated tax payments based on your yearly salary</p>
 
-                <button class="blue-button">Submit</button>
+                <div class="input-container">
+                    <label for="tax-input" class="input-label">Enter Yearly Salary:</label>
+                    <input type="number" id="tax-input" class="input-box" placeholder="Enter Yearly Salary">
+                    <label for="tax-input" class="input-label">Additional:</label>
+                    <input type="number" id="tax-input" class="input-box" placeholder="Enter Bonus (Optional)">
+                    <button class="blue-button">Submit</button>
+                </div>
+           </div>
+
+            <div class = "rounded-rectangle2">
+             <h2 class = "title">Tax Estimation</h2>
+             <p class="description">**Note this is only based on the standard deduction and no other contributions**</p>
+
+                <div class="input-container">
+                    <label for="tax-input" class="input-label">Tax Amount:</label>
+                    <input type="text" id="tax-input" class="input-box" placeholder="Enter text...">
+                    <label for="tax-input" class="input-label">Something:</label>
+                    <input type="text" id="tax-input" class="input-box" placeholder="Enter text...">
+                    <button class="blue-button">Submit</button>
+                </div>
+
             </div>
-          </div>
 
-          <div class = "rounded-rectangle2">
+        
 
-          </div>
 
-         </body>
+         </div>
+        
 
-       
+        
+        
+        </body>
 
         `;
 

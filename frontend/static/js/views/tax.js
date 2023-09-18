8bd4afd621e5eea552a9ef2097893b8d24287d67
James <101947821+pmjamez@users.noreply.github.com>
2023-09-18 09:17:41 -0700

    added rest of output value boxes, added background image in assets

--- frontend/static/js/views/tax.js
+++ frontend/static/js/views/tax.js
@@ -92,7 +92,9 @@ export default class extends AbstractView{
                      </select>
                     <label for="bonus-input" class="input-label">Additional (Optional):</label>
                     <input type="number" id="bonus-input" class="input-box" placeholder="Enter Bonus">
-                    <input type="number" id="contribution-input" class="input-box" placeholder="Contributions (401K, HSA, 457b)">
+                    <input type="number" id="contribution-input" class="input-box" placeholder="Contributions (401K*, HSA**, 457b***)">
+                    <label for="dependent-input" class="input-label"></label>
+                    <input type = "number" id="dependent-input" class="input-box" placeholder="# of Dependents" step = "any">
                     <button id = "submit-button" class="blue-button">Submit</button>
                 </div>
            </div>
@@ -104,6 +106,20 @@ export default class extends AbstractView{
                 <div class="input-container">
                     <label for="tax-result" class="input-label">Total Yearly Tax Amount:</label>
                     <input type="text" id="tax-result" class="input-box" placeholder=""disabled>
+                    <label for="fed-tax-result" class="input-label">Federal Tax:</label>
+                    <input type="text" id="fed-tax-result" class="input-box" placeholder=""disabled>
+                    <label for="state-tax-result" class="input-label">State Tax:</label>
+                    <input type="text" id="state-tax-result" class="input-box" placeholder=""disabled>
+                    <label for="social-tax-result" class="input-label">Social Security Tax:</label>
+                    <input type="text" id="social-tax-result" class="input-box" placeholder=""disabled>
+                    <label for="medi-tax-result" class="input-label">Medicare Tax:</label>
+                    <input type="text" id="medi-tax-result" class="input-box" placeholder=""disabled>
+                    <label for="takehome-pay" class="input-label">Yearly Take-Home Pay:</label>
+                    <input type="text" id="takehome-pay" class="input-box" placeholder=""disabled>
+                    <label for="monthtakehome-pay" class="input-label">Monthly Take-Home Pay:</label>
+                    <input type="text" id="monthtakehome-pay" class="input-box" placeholder=""disabled>
+               
+                    
                     
                   
                 </div>

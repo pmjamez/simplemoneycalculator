f2bd60502704deca2d51562c4ba22b45f51b25a7
James <101947821+pmjamez@users.noreply.github.com>
2023-10-08 16:35:28 -0700

    updated all state tax data

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -61,6 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
     router();
 });
 
+
 function AGI(taxInput, statusInput, contributionInput, dependentInput){
     const singleStandardDeduction = 13850;
     const headStandardDeduction = 20800;
@@ -98,10 +99,18 @@ function CalculateFederalTax(AdjustedIncome, statusInput){
 
     let incomeAmounts = [];
 
+    let taxbracket1 = 0;
+    let taxbracket2 = 11000;
+    let taxbracket3 = 42745;
+    let taxbracket4 = 95375;
+    let taxbracket5 = 182100;
+    let taxbracket6 = 231250;
+    let taxbracket7 = 578125;
+
     if (statusInput === 'Single' || statusInput === 'MarriedSep') {
-        incomeAmounts = [0, 11000, 44725, 95375, 182100, 231250, 578125];
+        incomeAmounts = [taxbracket1,taxbracket2,taxbracket3,taxbracket4,taxbracket5,taxbracket6,taxbracket7];
     } else if (statusInput === 'Married') {
-        incomeAmounts = [0, 22000, 89450, 190750, 364200, 462500, 693750];
+        incomeAmounts = [taxbracket1,taxbracket2*2,taxbracket3*2,taxbracket4*2,taxbracket5*2,taxbracket6*2,taxbracket7*2];
     } else if (statusInput === "Head of Household") {
         incomeAmounts = [0, 15700, 59850, 95350, 182100, 2312250, 578100];
     } else {
@@ -129,252 +138,365 @@ function CalculateStateTax(AdjustedIncome, stateInput){
         {
             name: "AL",
             brackets: [0.02, 0.04, 0.05],
-            amounts: [0, 500, 3000],
+            amounts: [500, 2500, 3000],
+            deduction: [3000, 8500],
+            dependent: [1000]
         },
         {
             name: "AK",
             brackets: [0.0],
             amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "AZ",
-            brackets: [0.0259, 0.0334, 0.0417, 0.0450, 0.0490, 0.0590, 0.0800],
-            amounts: [10318, 25872, 51744, 155232, 517440, 1552320],
+            brackets: [0.025],
+            amounts: [0],
+            deduction: [13850, 27700],
+            dependent: [100]
         },
         {
             name: "AR",
-            brackets: [0.02, 0.04, 0.05, 0.0590, 0.0660],
-            amounts: [4299, 8499, 12699, 21165],
+            brackets: [0.02, 0.04, 0.049],
+            amounts: [0, 4300, 8500],
+            deduction: [2270, 4540],
+            dependent: [29]
         },
         {
             name: "CA",
-            brackets: [0.01, 0.02, 0.04, 0.06, 0.08, 0.093, 0.103, 0.113, 0.123],
-            amounts: [8542, 20255, 31969, 44377, 56085, 286492, 343791],
+            brackets: [0.01, 0.02, 0.04, 0.06, 0.08, 0.093, 0.103, 0.113, 0.123, 0.133],
+            amounts: [0, 10099, 23942, 37788, 52454, 66295, 338639, 406364, 677275, 1000000],
+            deduction: [5202, 10404],
+            dependent: [433]
         },
         {
             name: "CO",
-            brackets: [0.0450],
-            amounts: null,
+            brackets: [0.044],
+            amounts: [0],
+            deduction: [13850],
+            dependent: [0]
         },
         {
             name: "CT",
-            brackets: [0.03, 0.05, 0.055, 0.06, 0.065, 0.069],
-            amounts: [10000, 50000, 100000, 200000],
+            brackets: [0.03, 0.05, 0.055, 0.06, 0.065, 0.069, 0.0699],
+            amounts: [0, 10000, 50000, 100000, 200000, 250000, 500000],
+            deduction: [15000, 24000],
+            dependent: [0]
         },
         {
             name: "DE",
-            brackets: [0.022, 0.039, 0.048, 0.052, 0.0555, 0.0595, 0.066, 0.0705, 0.078],
+            brackets: [0.022, 0.039, 0.048, 0.052, 0.0555, 0.066],
             amounts: [2000, 5000, 10000, 20000, 25000, 60000],
+            deduction: [3250, 6500],
+            dependent: [110]
         },
         {
             name: "FL",
-            brackets: [0.04, 0.055, 0.06, 0.065, 0.07, 0.075],
-            amounts: [5000, 25000, 50000, 75000, 100000],
+            brackets: [0.0],
+            amounts: [0],
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "GA",
-            brackets: [0.01, 0.02, 0.03, 0.04, 0.05, 0.0575, 0.06],
-            amounts: [750, 2250, 3750, 5250, 7000],
+            brackets: [0.01, 0.02, 0.03, 0.04, 0.05, 0.0575],
+            amounts: [0, 750, 2250, 3750, 5250, 7000],
+            deduction: [5400, 7100],
+            dependent: [3000]
         },
         {
             name: "HI",
-            brackets: [0.014, 0.032, 0.055, 0.064, 0.068, 0.072, 0.076, 0.079, 0.0825],
-            amounts: [2400, 4800, 9600, 14400, 19200],
+            brackets: [0.014,0.032,0.055,0.064,0.068,0.072,0.076,0.079,0.0825,0.09,0.1,0.11],
+            amounts: [0,2400,4800,9600,14400,19200,24000,36000,48000,150000,175000,200000],
+            deduction: [2200,4400],
+            dependent: [1144]
         },
         {
             name: "ID",
-            brackets: [0.01125, 0.03125, 0.0325, 0.035, 0.04, 0.05, 0.06, 0.06925],
-            amounts: [1550, 3100, 4650, 6200, 7750],
+            brackets: [0.058],
+            amounts: [0],
+            deduction: [13850,27700],
+            dependent: [0]
         },
         {
             name: "IL",
             brackets: [0.0495],
-            amounts: null,
+            amounts: [0],
+            deduction: [2425, 2850],
+            dependent: [2425]
         },
         {
             name: "IN",
-            brackets: [0.0323, 0.0333, 0.034, 0.0323],
-            amounts: [10000, 25000, 50000],
+            brackets: [0.0315],
+            amounts: [0],
+            deduction: [1000,2000],
+            dependent: [1000]
         },
         {
             name: "IA",
-            brackets: [0.0033, 0.0067, 0.0225, 0.0414, 0.0563, 0.0667],
-            amounts: [1000, 5000, 10000, 20000],
+            brackets: [0.044, 0.0482,0.057,0.06],
+            amounts: [0,6000,30000,75000],
+            deduction: [40,80],
+            dependent: [40]
         },
         {
             name: "KS",
-            brackets: [0.031, 0.0525],
-            amounts: [15000, 30000],
+            brackets: [0.031,0.0525,0.057],
+            amounts: [0,15000,30000],
+            deduction: [3500,8000],
+            dependent: [2250]
         },
         {
             name: "KY",
-            brackets: [0.02, 0.03, 0.04, 0.05, 0.058, 0.06],
-            amounts: [3000, 4000, 5000, 5800, 6000],
+            brackets: [0.045],
+            amounts: [0],
+            deduction: [2770,5540],
+            dependent: []
         },
         {
             name: "LA",
-            brackets: [0.02, 0.04, 0.06],
-            amounts: [12500, 50000],
+            brackets: [0.0185,0.035,0.0425],
+            amounts: [0,12500, 50000],
+            deduction: [4500,9000],
+            dependent: [1000]
         },
         {
             name: "ME",
-            brackets: [0.058, 0.0675],
-            amounts: [21050],
+            brackets: [0.058,0.0675,0.0715],
+            amounts: [0,24500,5805],
+            deduction: [13850,27700],
+            dependent: [300]
         },
         {
             name: "MD",
-            brackets: [0.02, 0.03, 0.04, 0.0475, 0.05, 0.0525, 0.055, 0.0575, 0.06],
-            amounts: [1000, 2000, 3000, 4000, 5000],
+            brackets: [0.02,0.03,0.04,0.0475,0.05,0.0525,0.055,0.0575],
+            amounts: [1000, 2000, 3000, 100000,125000,150000,250000],
+            deduction: [2400,4850],
+            dependent: [3200]
         },
         {
             name: "MA",
-            brackets: [0.05],
-            amounts: null,
+            brackets: [0.05,0.09],
+            amounts: [0,1000000],
+            deduction: [4400,8800],
+            dependent: [1000]
         },
         {
             name: "MI",
             brackets: [0.0425],
-            amounts: null,
+            amounts: [0.0],
+            deduction: [5000,10000],
+            dependent: [5000]
         },
         {
             name: "MN",
-            brackets: [0.0535, 0.0705, 0.0785, 0.0985],
-            amounts: [27090, 180251, 280251],
+            brackets: [0.0535, 0.068,0.0785,0.0985],
+            amounts: [0,30070,98760,183340],
+            deduction: [13825,27650],
+            dependent: [4800]
         },
         {
             name: "MS",
-            brackets: [0.03, 0.04, 0.05],
-            amounts: [5000, 10000],
+            brackets: [0.05],
+            amounts: [10000],
+            deduction: [6000,15000],
+            dependent: [1500]
         },
         {
             name: "MO",
-            brackets: [0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.054],
-            amounts: [1008, 2108, 4216, 8429, 10000],
+            brackets: [0.02,0.025,0.03,0.035,0.04,0.045,0.0495],
+            amounts: [1121,2242,3363,4484,5605,6726,7847],
+            deduction: [13850,27700],
+            dependent: [0]
         },
         {
             name: "MT",
-            brackets: [0.01, 0.02, 0.03, 0.04, 0.054, 0.069],
-            amounts: [2900, 5800, 8700, 11599],
+            brackets: [0.01, 0.02, 0.03, 0.04, 0.05, 0.06,0.0675],
+            amounts: [0,6300,9700,13000,16800,21600],
+            deduction: [5540,11080],
+            dependent: [2960]
         },
         {
             name: "NE",
-            brackets: [0.0246, 0.0322, 0.0357, 0.0501, 0.0684],
-            amounts: [3200, 16400, 29750, 31900],
+            brackets: [0.0246,0.0351,0.0501,0.0664],
+            amounts: [0,3700,22170,35730],
+            deduction: [7900,15800],
+            dependent: [157]
         },
         {
             name: "NV",
             brackets: [0.0],
             amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "NH",
             brackets: [0.0],
             amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "NJ",
-            brackets: [0.014, 0.0175, 0.0245, 0.035, 0.0553, 0.0637, 0.0897],
-            amounts: [20000, 35000, 40000, 75000, 500000],
+            brackets: [0.014, 0.0175, 0.035,0.0552,0.0637,0.0897,0.107],
+            amounts: [0,20000, 35000, 40000, 75000, 500000,1000000],
+            deduction: [1000,2000],
+            dependent: [1500]
         },
         {
             name: "NM",
             brackets: [0.017, 0.032, 0.047, 0.049, 0.059],
-            amounts: [5500, 11000, 16000],
+            amounts: [0, 5500, 11000, 16000, 210000],
+            deduction: [13850,27700],
+            dependent: [4000]
         },
         {
             name: "NY",
-            brackets: [0.04, 0.045, 0.0525, 0.059, 0.0633, 0.0657, 0.0685, 0.0882, 0.0965],
-            amounts: [8500, 11700, 13900, 21400, 80650, 215400, 1077550],
+            brackets: [0.04, 0.045, 0.0525, 0.055,0.06,0.0685,0.00965,0.103,0.109],
+            amounts: [8500, 11700, 13900, 80650, 215400, 1077550,5000000, 25000000],
+            deduction: [8000,16500],
+            dependent: [1000]
         },
         {
             name: "NC",
-            brackets: [0.0525],
+            brackets: [0.0475],
             amounts: null,
+            deduction: [12750,25500],
+            dependent: [0]
         },
         {
             name: "ND",
             brackets: [0.011, 0.0204, 0.0227, 0.0264, 0.029],
-            amounts: [3950, 9775, 39475],
+            amounts: [0,41775,101050,210825,458350],
+            deduction: [13850,27700],
+            dependent: [0]
         },
         {
             name: "OH",
-            brackets: [0.005, 0.01, 0.02, 0.025, 0.03, 0.035, 0.045],
-            amounts: [10750, 21500, 43000],
+            brackets: [0.0276,0.03222,0.0368,0.0399],
+            amounts: [26050,46100,92150,115300],
+            deduction: [2400,4800],
+            dependent: [2400]
         },
         {
             name: "OK",
-            brackets: [0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.0525, 0.055, 0.0565],
-            amounts: [1000, 5000, 10000, 20000, 25000, 50000],
+            brackets: [0.0025,0.0075,0.0175,0.0275,0.0375,0.0475],
+            amounts: [0,1000,2500,3750,4900,7200],
+            deduction: [6350,12700],
+            dependent: [1000]
         },
         {
             name: "OR",
-            brackets: [0.05, 0.07, 0.09, 0.099, 0.108, 0.11, 0.1125],
-            amounts: [3400, 15000, 20000, 125000],
+            brackets: [0.0475,0.0675,0.0875,0.099],
+            amounts: [0,4050,10200,125000],
+            deduction: [2605,5210],
+            dependent: [236]
         },
         {
             name: "PA",
             brackets: [0.0307],
-            amounts: null,
+            amounts: [0],
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "RI",
-            brackets: [0.0375, 0.0475, 0.0599, 0.059],
-            amounts: [6150, 15750, 67450],
+            brackets: [0.0375,0.0475,0.0599],
+            amounts: [0,68200,155000],
+            deduction: [10000,20050],
+            dependent: [4700]
         },
         {
             name: "SC",
-            brackets: [0.03, 0.04, 0.05, 0.06, 0.07],
-            amounts: [3000, 6000, 12000, 17000],
+            brackets: [0.03, 0.065],
+            amounts: [3200,16040],
+            deduction: [13850,27700],
+            dependent: [4430]
         },
         {
             name: "SD",
             brackets: [0.0],
             amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "TN",
-            brackets: [0.01, 0.02, 0.03, 0.04, 0.045, 0.05, 0.06],
-            amounts: [10000, 20000, 30000, 45000],
+            brackets: [0.0],
+            amounts: null,
+            deduction: [0],
+            dependent: [0]
+        
         },
         {
             name: "TX",
-            brackets: [0.0, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03],
-            amounts: [5000, 10000, 15000, 20000],
+            brackets: [0.0],
+            amounts: null,
+            deduction: [0],
+            dependent: [0]
+        
         },
         {
             name: "UT",
             brackets: [0.0495],
             amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "VT",
             brackets: [0.0335, 0.066, 0.076, 0.0875, 0.0895],
             amounts: [39150, 195750, 260250],
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "VA",
             brackets: [0.02, 0.03, 0.0575],
             amounts: [3000, 5000, 17000],
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "WA",
             brackets: [0.0],
             amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "WV",
             brackets: [0.03, 0.04, 0.045, 0.06],
             amounts: [10000, 25000, 40000],
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "WI",
-            brackets: [0.0354, 0.0465, 0.062]
+            brackets: [0.0354, 0.0465, 0.062],
+            amounts: null,
+            deduction: [0],
+            dependent: [0]
         },
         {
             name: "WY",
             brackets: [0.0],
             amounts: null,
-        }
+            deduction: [0],
+            dependent: [0],
+
+        },
+        {
+            name: "DC",
+            brackets: [0.0],
+            amounts: null,
+            deduction: [0],
+            dependent: [0],
+
+        },
+       
     ]
     console.log(stateInput);
 
